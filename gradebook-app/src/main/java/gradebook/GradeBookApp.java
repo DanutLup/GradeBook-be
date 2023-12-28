17cf61b740d09914b1573860637f744307319f4d
Danut Lup <danut.lup2002@gmail.com>
2023-12-28 10:58:22 +0200

    first changes

---
+++ gradebook-app/src/main/java/gradebook/GradeBookApp.java
@@ -0,0 +1,13 @@
+package gradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeBookApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeBookApp.class, args);
+    }
+
+}
